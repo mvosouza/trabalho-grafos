628ee5efee742b648c31347b3db4bbfa36a8b9d5
fbenev <fbenev@gmail.com>
2011-05-25 16:03:23 +0000

    refatorado classe aresta

--- src/main/java/br/unifor/grafos/algoritmos/Aresta.java
+++ src/main/java/br/unifor/grafos/algoritmos/Aresta.java
@@ -2,24 +2,24 @@ package br.unifor.grafos.algoritmos;
 
 public class Aresta {
 
-	private int v, a, valor;
+	private int origem, destino, valor;
 
-	public Aresta(int v, int a, int valor){
-		this.v=v;
-		this.a=a;
-		this.valor=valor;
+	public Aresta(int v, int a, int valor) {
+		this.origem = v;
+		this.destino = a;
+		this.valor = valor;
 	}
 
 	public int v() {
-		return v;
+		return origem;
 	}
 
 	public int a() {
-		return a;
+		return destino;
 	}
 
 	public int valor() {
 		return valor;
 	}
-}
 
+}
