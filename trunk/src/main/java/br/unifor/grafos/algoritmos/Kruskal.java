d2bad832c816fdc76022c6e7a7e7b5b2c2d53401
fbenev@gmail.com <fbenev@gmail.com@fc9cb52d-c050-2f54-ca74-1b480d7b784d>
2011-06-01 17:42:42 +0000

    versao final

--- trunk/src/main/java/br/unifor/grafos/algoritmos/Kruskal.java
+++ trunk/src/main/java/br/unifor/grafos/algoritmos/Kruskal.java
@@ -5,164 +5,158 @@ import java.util.Iterator;
 import java.util.TreeSet;
 import java.util.Vector;
 
-/**
- * Algoritmo de Kruskal
- * 
- * @author Daniel Alves
- *
- */
-
 public class Kruskal {
-	
-	private final int MAXIMO_NOS = 999;
-	private HashSet nos[]; // Array of connected components
-	private TreeSet todasArestas; // Priority queue of Edge objects
-	private Vector novasArestas; // Edges in Minimal-Spanning Tree
 
-	Kruskal() {
-		nos = new HashSet[MAXIMO_NOS]; // Create array for components
-		todasArestas = new TreeSet(new Aresta()); // Create empty priority queue
-		novasArestas = new Vector(MAXIMO_NOS); // Create vector for MST edges
+	private final Integer MAX = 999;
+
+	private final Grafo grafo;
+
+	private HashSet<Integer>[] florestaDeComponentes;
+
+	private TreeSet<Aresta> todasAsArestas;
+	
+	private Vector<Aresta> arvoreMST;
+
+	public Kruskal(final Grafo grafo) {
+		this.grafo = grafo;
+		this.florestaDeComponentes = new HashSet[MAX];
+		this.todasAsArestas = new TreeSet<Aresta>(new Aresta());
+		this.arvoreMST = new Vector<Aresta>(MAX);
+
+		this.criaConjuntoDeArestasEFlorestaDeComponentes();
 	}
 
-	public int iniciar(int num_nodes, int vertices[], Integer matriz[][]) {
+	private void criaConjuntoDeArestasEFlorestaDeComponentes() {
+		Integer[][] matrizDeAdjacencia = grafo.getMatrizDeAdjacencia();
 
-		// Cria os nos
-		int origem, destino, valor;
-		for (int i = 0; i < num_nodes; i++) {
-			for (int j = 0; j < num_nodes; j++) {
-				origem = i;
-				destino = j;
-				valor = matriz[i][j];
-				todasArestas.add(new Aresta(origem, destino, valor)); // Update
-																		// priority
-																		// queue
-				if (nos[origem] == null) {
-					// Create set of connect components [singleton] for this
-					// node
-					nos[origem] = new HashSet(2 * MAXIMO_NOS);
-					nos[origem].add(new Integer(origem));
+		for (int i = 0; i < grafo.getNumeroDeVertices(); i++) {
+			for (int j = 0; j < grafo.getNumeroDeVertices(); j++) {
+				Aresta aresta = new Aresta(i, j, matrizDeAdjacencia[i][j]);
+				this.todasAsArestas.add(aresta);
+
+				if (florestaDeComponentes[aresta.origem()] == null) {
+					florestaDeComponentes[aresta.origem()] = new HashSet<Integer>(
+							2 * MAX);
+					florestaDeComponentes[aresta.origem()].add(aresta.origem());
 				}
 
-				if (nos[destino] == null) {
-					// Create set of connect components [singleton] for this
-					// node
-					nos[destino] = new HashSet(2 * MAXIMO_NOS);
-					nos[destino].add(new Integer(destino));
+				if (florestaDeComponentes[aresta.destino()] == null) {
+					florestaDeComponentes[aresta.destino()] = new HashSet<Integer>(
+							2 * MAX);
+					florestaDeComponentes[aresta.destino()].add(aresta
+							.destino());
 				}
 			}
 		}
-
-		// Iterecao Principal de Kruskal
-		int size = todasArestas.size();
-		for (int i = 0; i < size; i++) {
-			Aresta arestaAtual = (Aresta) todasArestas.first();
-			if (todasArestas.remove(arestaAtual)) {
-				// successful removal from priority queue: allEdges
-
-				if (nosEstaoEmSetsDiferentes(arestaAtual.origem(),
-						arestaAtual.destino())) {
-					// System.out.println("Nodes are in different sets ...");
-					HashSet hsetOrigem, hSetDestino;
-					int destinoHashSetIndex;
-
-					if (nos[arestaAtual.origem()].size() > nos[arestaAtual
-							.destino()].size()) {
-						// have to transfer all nodes including curEdge.to
-						hsetOrigem = nos[arestaAtual.destino()];
-						hSetDestino = nos[destinoHashSetIndex = arestaAtual
-								.origem()];
-					} else {
-						// have to transfer all nodes including curEdge.from
-						hsetOrigem = nos[arestaAtual.origem()];
-						hSetDestino = nos[destinoHashSetIndex = arestaAtual
-								.destino()];
-					}
-
-					Object arrayOrigem[] = hsetOrigem.toArray();
-					int transferSize = arrayOrigem.length;
-					for (int j = 0; j < transferSize; j++) {
-						// move each node from set: src into set: dst
-						// and update appropriate index in array: nodes
-						if (hsetOrigem.remove(arrayOrigem[j])) {
-							hSetDestino.add(arrayOrigem[j]);
-							nos[((Integer) arrayOrigem[j]).intValue()] = nos[destinoHashSetIndex];
-						} else {
-							// This is a serious problem
-							System.out.println("Something wrong: set union");
-							System.exit(1);
-						}
-					}
-
-					novasArestas.add(arestaAtual);
-					// add new edge to MST edge vector
-				} else {
-					// System.out.println("Nodes are in the same set ... nothing to do here");
-				}
-
-			} else {
-				// This is a serious problem
-				System.out
-						.println("TreeSet should have contained this element!!");
-				System.exit(1);
-			}
-			imprimeSolucaoParcial();
-			imprimeFilaDePrioridade();
-		}
-
-		return retornaCusto();
 	}
-	
-	/**
-	 * Imprime as novas arestas.
-	 */
 
-	public void imprimeSolucaoParcial() {
-		// imprime all new Edges
-		System.out
-				.println("**** Imprimindo Solu��o Parcial **** Conjuntos disjuntos da floresta \n");
-		for (int i = 0; i < novasArestas.size(); i++) {
-			System.out.println("Aresta : " + i + " com peso - "
-					+ ((Aresta) novasArestas.get(i)).valor());
-		}
-		System.out
-				.println("**** Imprimindo Solu��o Parcial **** Conjuntos disjuntos da floresta \n");
+	private boolean arestaConectaDuasArvoresDiferentes(Aresta aresta) {
+		return !florestaDeComponentes[aresta.origem()]
+				.equals(florestaDeComponentes[aresta.destino()]);
 	}
-	
-	/**
-	 * Imprime a fila de prioridades.
-	 */
 
-	public void imprimeFilaDePrioridade() {
-		System.out.println("**** Imprimindo Fila de Prioridade **** \n");
-		Iterator i = todasArestas.iterator();
+	private void combinarDuasArvoresEmUma(Aresta aresta) {
+		HashSet<Integer> arvoresOrigem;
+		HashSet<Integer> arvoresDestino;
+
+		int indiceDaArvore;
+
+		if (florestaDeComponentes[aresta.origem()].size() > florestaDeComponentes[aresta.destino()].size()) {
+			indiceDaArvore = aresta.origem();
+
+			arvoresOrigem = florestaDeComponentes[aresta.destino()];
+			arvoresDestino = florestaDeComponentes[indiceDaArvore];
+
+		} else {
+			indiceDaArvore = aresta.destino();
+			
+			arvoresOrigem = florestaDeComponentes[aresta.origem()];
+			arvoresDestino = florestaDeComponentes[indiceDaArvore];
+		}
 		
-		int contador = 0;
-		while (i.hasNext()) {
-			System.out.println("Aresta : " + contador + " com peso - "
-					+ ((Aresta) i.next()).valor());
-			contador++;
+		Object[] origens = arvoresOrigem.toArray();
+		
+		for (int i = 0; i < origens.length; i++) {
+			
+			int j = (Integer)origens[i];
+			
+			if (arvoresOrigem.remove(origens[i])) {
+				arvoresDestino.add(j);
+				florestaDeComponentes[j] = florestaDeComponentes[indiceDaArvore];
+			}
 		}
-		System.out.println("**** Imprimindo Fila de Prioridade **** \n");
-
+	}
+	
+	private void imprimeArvoreMST() {
+		Aresta aresta;
+		
+		System.out.println("\nConjuntos disjuntos da floresta...");
+		System.out.println("----------");
+		
+		for (int i = 0; i < arvoreMST.size(); i++) {
+			aresta = arvoreMST.get(i);
+			
+			if (aresta.valor() != 0 && aresta.valor() != Integer.MAX_VALUE) {
+				String temp = "(" + aresta.origem() + ", " + aresta.destino() + ")";
+				
+				System.out.println("Aresta " + i + " " + temp + " com peso: " + aresta.valor());
+			}
+		}
+	}
+	
+	private void imprimeTodasArestas() {
+		Iterator<Aresta> it = todasAsArestas.iterator();
+		
+		Aresta aresta;
+		
+		System.out.println("\nImprimindo fila de prioridade...");
+		System.out.println("----------");
+		
+		while (it.hasNext()) {
+			aresta = it.next();
+			
+			if (aresta.valor() != 0 && aresta.valor() != Integer.MAX_VALUE) {
+				String temp = "(" + aresta.origem() + ", " + aresta.destino() + ")";
+				
+				System.out.println("Aresta " + temp + " com peso: " + aresta.valor());
+			}
+		}
+	}
+	
+	public Integer custoTotal() {
+		Integer custo = 0;
+		
+		Aresta arestaAtual;
+		
+		while (!arvoreMST.isEmpty()) {
+			arestaAtual = arvoreMST.firstElement();
+			
+			if (arvoreMST.remove(arestaAtual)) {
+				custo += arestaAtual.valor();
+			}
+		}
+		
+		return custo;
 	}
 
-	private boolean nosEstaoEmSetsDiferentes(int a, int b) {
-		// returns true if graph nodes (a,b) are in different
-		// connected components, ie the set for 'a' is different
-		// from that for 'b'
-		return (!nos[a].equals(nos[b]));
-	}
+	public void executar() {
+		while (!todasAsArestas.isEmpty()) {
 
-	private int retornaCusto() {
-		int i = 0;
-		while (!novasArestas.isEmpty()) {
-			// for each edge in Vector of MST edges
-			Aresta e = (Aresta) novasArestas.firstElement();
-			i = i + e.valor();
-			novasArestas.remove(e);
+			Aresta arestaAtual = todasAsArestas.first();
+
+			if (todasAsArestas.remove(arestaAtual)) {
+
+				if (arestaConectaDuasArvoresDiferentes(arestaAtual)) {
+					combinarDuasArvoresEmUma(arestaAtual);
+					this.arvoreMST.add(arestaAtual);
+				}
+				
+				imprimeArvoreMST();
+				imprimeTodasArestas();
+			}
 		}
-		return i;
+		
+		System.out.println("\nCusto total: " + custoTotal());
 	}
 
 }
